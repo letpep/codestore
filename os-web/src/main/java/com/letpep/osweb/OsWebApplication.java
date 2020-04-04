3d6997db8179907e95900ae4ef271be40d56bb62
李信 <li.xin@sf-express.com>
2020-04-04 13:15:27 +0800

    CODE INIT

---
+++ os-web/src/main/java/com/letpep/osweb/OsWebApplication.java
@@ -0,0 +1,13 @@
+package com.letpep.osweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsWebApplication.class, args);
+	}
+
+}
---
+++ os-web/src/test/java/com/letpep/osweb/OsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letpep.osweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
